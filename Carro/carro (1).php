1e0ae4a848e25b8e1dbf2775bcbdb1fc80dcba06
Leobatman007 <leoccampos270722@gmail.com>
2025-06-19 13:26:44 -0300

    Add files via upload

---
+++ Carro/carro (1).php
@@ -0,0 +1,41 @@
+<?php
+class Carro {
+    private $modelo;
+    private $cor;
+    private $ano;
+    
+    public function __construct($modelo, $cor, $ano) {
+        $this->modelo = $modelo;
+        $this->cor = $cor;
+        $this->ano = $ano;
+    }
+    public function getModelo() {
+        return $this->modelo;
+    }
+    public function setModelo($modelo) {
+        $this->modelo = $modelo;
+    }
+    public function getCor() {
+        return $this->cor;
+    }
+    public function setCor($cor) {
+        $this->cor = $cor;
+    }
+    public function getAno() {
+        return $this->ano;
+    }
+    public function setAno($ano) {
+        $this->ano = $ano;
+    }
+}
+?>
+<?php
+$carro = new Carro("fusca", "azul", 2013);
+echo $carro->getModelo(); 
+echo "\n";
+echo $carro->getCor();    
+echo "\n";
+echo $carro->getAno();   
+$carro->setCor("Vermelho");
+echo "\nNova cor: " . $carro->getCor();
+?>
