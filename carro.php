4e16da43a716b218905a24bc2a6aaad7883f1adb
Leobatman007 <leoccampos270722@gmail.com>
2025-06-19 11:59:00 -0300

    Update carro.php

--- carro.php
+++ carro.php
@@ -3,37 +3,27 @@ class Carro {
     private $modelo;
     private $cor;
     private $ano;
-
     
     public function __construct($modelo, $cor, $ano) {
         $this->modelo = $modelo;
         $this->cor = $cor;
         $this->ano = $ano;
     }
-
-
     public function getModelo() {
         return $this->modelo;
     }
-
     public function setModelo($modelo) {
         $this->modelo = $modelo;
     }
-
-    
     public function getCor() {
         return $this->cor;
     }
-
     public function setCor($cor) {
         $this->cor = $cor;
     }
-
-    
     public function getAno() {
         return $this->ano;
     }
-
     public function setAno($ano) {
         $this->ano = $ano;
     }
@@ -41,13 +31,11 @@ class Carro {
 ?>
 <?php
 $carro = new Carro("fusca", "azul", 2013);
-
 echo $carro->getModelo(); 
 echo "\n";
 echo $carro->getCor();    
 echo "\n";
 echo $carro->getAno();   
-
 $carro->setCor("Vermelho");
 echo "\nNova cor: " . $carro->getCor();
 ?>
